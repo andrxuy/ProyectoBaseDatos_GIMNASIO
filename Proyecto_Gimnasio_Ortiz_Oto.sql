e50b5f68dd0614a80e2e69f2421c14b93307c0a9
andrxuy <andresoto198@gmail.com>
2026-01-05 18:20:05 -0500

    Add files via upload

---
+++ Proyecto_Gimnasio_Ortiz_Oto.sql
@@ -0,0 +1,14 @@
+-- Database: Proyecto_Gimnasio_Ortiz_Oto
+
+-- DROP DATABASE IF EXISTS "Proyecto_Gimnasio_Ortiz_Oto";
+
+CREATE DATABASE "Proyecto_Gimnasio_Ortiz_Oto"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Ecuador.1252'
+    LC_CTYPE = 'Spanish_Ecuador.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
